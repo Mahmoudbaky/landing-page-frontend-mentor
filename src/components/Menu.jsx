a396f88c3dba51c32b28956ae8409b35fc1ab962
Mahmoudbaky <ma7mouudbaky@gmail.com>
2025-01-08 23:42:53 +0300

    done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,39 +1,131 @@
-const Menu = () => {
-  const content = [
-    {
-      id: 1,
-      title: "Todo",
-      icon: "src/assets/images/icon-todo.svg",
-    },
-    {
-      id: 2,
-      title: "Calendar",
-      icon: "src/assets/images/icon-calendar.svg",
-    },
-    {
-      id: 3,
-      title: "Reminders",
-      icon: "src/assets/images/icon-reminders.svg",
-    },
-    {
-      id: 4,
-      title: "Planning",
-      icon: "src/assets/images/icon-planning.svg",
-    },
-  ];
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import FeaturesMenu from "./FeaturesMenu";
+import CompanyMenu from "./CompanyMenu";
+import { HiOutlineMenu } from "react-icons/hi";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
+const Menu = () => {
+  const [menu, setMenu] = useState(false);
+  const [company, setCompany] = useState(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+
+  const handleCompany = () => {
+    setCompany(!company);
+  };
   return (
-    <ul className="w-full p-3 rounded-lg shadow-lg rounded-box">
-      {content.map((c) => {
-        return (
-          <li key={c.id} className="flex items-center gap-2 p-2">
-            <img src={`${c.icon}`} alt="icon" />
-            <a>{c.title}</a>
+    <div className="flex flex-row items-center w-full justify-between gap-[30px] py-5 px-5">
+      <div className="flex flex-row items-center w-full justify-between gap-[30px]">
+        <ul className="hidden lg:flex flex-row gap-[15px] font-semibold opacity-[0.7] ">
+          <li>
+            <div
+              onClick={handleMenu}
+              className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
+            >
+              <p>Features</p>
+              <div>{menu ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
+              {menu && (
+                <div className="absolute left-[150px] top-[80px] rounded-lg bg-white ">
+                  <FeaturesMenu />
+                </div>
+              )}
+            </div>
           </li>
-        );
-      })}
-    </ul>
+          <li>
+            <div
+              onClick={handleCompany}
+              className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
+            >
+              <p>Company</p>
+              {company ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              {company && (
+                <div className="absolute left-[350px] top-[80px] bg-white">
+                  <CompanyMenu />
+                </div>
+              )}
+            </div>
+          </li>
+          <li className="hover:opacity-100 hover:cursor-pointer">careers</li>
+          <li className="hover:opacity-100 hover:cursor-pointer">About</li>
+        </ul>
+        <div className=" hidden lg:flex flex-row gap-[20px] opacity-[0.7]">
+          <button>Login</button>
+          <button className="border-black border-[0.5px] py-2 px-4 rounded-xl">
+            Resgister
+          </button>
+        </div>
+      </div>
+
+      <nav className="mr-0 lg:hidden">
+        <Sheet>
+          <SheetTrigger className="text-3xl ">
+            <HiOutlineMenu />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetTitle>Menu</SheetTitle>
+            <ul className="flex flex-col gap-[15px] font-semibold opacity-[0.7] pt-5">
+              <li>
+                <div
+                  onClick={handleMenu}
+                  className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
+                >
+                  <p>Features</p>
+                  <div>{menu ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
+                  {menu && (
+                    <div className="absolute left-[150px] top-[80px] rounded-lg bg-white ">
+                      <FeaturesMenu />
+                    </div>
+                  )}
+                </div>
+              </li>
+              <li>
+                <div
+                  onClick={handleCompany}
+                  className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
+                >
+                  <p>Company</p>
+                  {company ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                  {company && (
+                    <div className="absolute left-[350px] top-[80px] bg-white">
+                      <CompanyMenu />
+                    </div>
+                  )}
+                </div>
+              </li>
+              <li className="hover:opacity-100 hover:cursor-pointer">
+                careers
+              </li>
+              <li className="hover:opacity-100 hover:cursor-pointer">About</li>
+            </ul>
+            <div className=" flex flex-col pt-5 gap-[20px] opacity-[0.7]">
+              <button>Login</button>
+              <button className="border-black border-[0.5px] py-2 px-4 rounded-xl">
+                Resgister
+              </button>
+            </div>
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
   );
 };
 
 export default Menu;
+
+{
+  /* <div className="text-3xl lg:hidden hover:cursor-pointer">
+          
+        </div> */
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,90 +1,16 @@
-import { HiOutlineMenu } from "react-icons/hi";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoIosArrowUp } from "react-icons/io";
-import { useState } from "react";
 import Menu from "./Menu";
-import CompanyMenu from "./CompanyMenu";
-import MobileMenu from "./MobileMenu";
-
 // i must use mx-auto with container to prevent the scroll effect
 
 const NavBar = () => {
-  const [menu, setMenu] = useState(false);
-  const [company, setCompany] = useState(false);
-  const [mobileMenu, setMobileMenu] = useState(false);
-
-  const handleMenu = () => {
-    setMenu(!menu);
-  };
-
-  const handleCompany = () => {
-    setCompany(!company);
-  };
-
-  const handleMobileMenu = () => {
-    setMobileMenu(!mobileMenu);
-  };
-
   return (
     <section>
-      <div className="container flex flex-row items-center justify-between pt-7">
-        <div className="flex flex-row gap-[30px]">
-          <img
-            src="src/assets/images/logo.svg"
-            alt="logo"
-            className="w-[100px]"
-          />
-
-          <ul className="hidden lg:flex flex-row gap-[15px] font-semibold opacity-[0.7] ">
-            <li>
-              <div
-                onClick={handleMenu}
-                className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
-              >
-                <p>Features</p>
-                <div>{menu ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
-                {menu && (
-                  <div className="absolute left-[200px] top-[60px] rounded-lg bg-white ">
-                    <Menu />
-                  </div>
-                )}
-              </div>
-            </li>
-            <li>
-              <div
-                onClick={handleCompany}
-                className="flex flex-row items-center gap-[3px] hover:opacity-100 hover:cursor-pointer"
-              >
-                <p>Company</p>
-                {company ? <IoIosArrowUp /> : <IoIosArrowDown />}
-                {company && (
-                  <div className="absolute left-[350px] top-[60px] bg-white">
-                    <CompanyMenu />
-                  </div>
-                )}
-              </div>
-            </li>
-            <li className="hover:opacity-100 hover:cursor-pointer">careers</li>
-            <li className="hover:opacity-100 hover:cursor-pointer">About</li>
-          </ul>
-        </div>
-        <div className=" hidden lg:flex flex-row gap-[20px] opacity-[0.7]">
-          <button>Login</button>
-          <button className="border-black border-[0.5px] py-2 px-4 rounded-xl">
-            Resgister
-          </button>
-        </div>
-        <div
-          onClick={handleMobileMenu}
-          className="text-3xl lg:hidden hover:cursor-pointer"
-        >
-          <HiOutlineMenu />
-        </div>
-        {mobileMenu && (
-          <div className="absolute top-[0px] right-[150px] bg-white rounded-lg p-4">
-            <MobileMenu />
-          </div>
-        )}
+      <div className="container flex flex-row items-center justify-between pt-7 b">
+        <img
+          src="src/assets/images/logo.svg"
+          alt="logo"
+          className="w-[100px]"
+        />
+        <Menu />
       </div>
     </section>
   );
